2a78911fc26d53ab8cbd899122d3c74ec24cde94
Urii Shalya <D.karaka@protonmail.com>
2019-11-16 21:41:18 +0200

    Initial Arcadex version

---
+++ Assets/Scripts/CharacterMoving.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMoving : MonoBehaviour
+{   
+    public float speed = 500f;
+    private Animator _anim;
+    private Rigidbody2D _body;
+
+    public float jumpForce = 3.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _body = GetComponent<Rigidbody2D>();
+        _anim = GetComponent<Animator>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float deltaX = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+        Vector2 movement = new Vector2 (deltaX, _body.velocity.y);
+        _body.velocity = movement;
+
+        if(Input.GetKeyDown(KeyCode.Space)){
+            _body.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+        }
+        _anim.SetFloat("speed", Mathf.Abs(deltaX));
+        transform.localScale = new Vector3 (Mathf.Sign(deltaX), 1, 1);
+    }
+}
