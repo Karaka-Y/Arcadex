911c8866b569da7e4342656cf3b28adcf37321bc
Urii Shalya <D.karaka@protonmail.com>
2019-11-24 17:31:30 +0200

    Added code for jumping and some scene blocks

--- Assets/Scripts/CharacterMoving.cs
+++ Assets/Scripts/CharacterMoving.cs
@@ -7,14 +7,14 @@ public class CharacterMoving : MonoBehaviour
     public float speed = 500f;
     private Animator _anim;
     private Rigidbody2D _body;
-
+    private BoxCollider2D _box;
     public float jumpForce = 3.0f;
     // Start is called before the first frame update
     void Start()
     {
         _body = GetComponent<Rigidbody2D>();
         _anim = GetComponent<Animator>();
-        
+        _box = GetComponent<BoxCollider2D>();
     }
 
     // Update is called once per frame
@@ -24,7 +24,20 @@ public class CharacterMoving : MonoBehaviour
         Vector2 movement = new Vector2 (deltaX, _body.velocity.y);
         _body.velocity = movement;
 
-        if(Input.GetKeyDown(KeyCode.Space)){
+        Vector3 max = _box.bounds.max;
+        Vector3 min = _box.bounds.min;
+
+        Vector2 Corner1 = new Vector2(max.x, min.y - 0.1f);
+        Vector2 Corner2 = new Vector2(min.x, min.y - .2f);
+
+        Collider2D hit = Physics2D.OverlapArea(Corner1, Corner2);
+        bool isGrounded = false;
+
+        if(hit != null){
+            isGrounded = true;
+        }
+
+        if(isGrounded && Input.GetKeyDown(KeyCode.Space)){
             _body.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
         }
         _anim.SetFloat("speed", Mathf.Abs(deltaX));
