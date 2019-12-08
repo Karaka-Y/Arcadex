26469ef1fed0b5725fa43f301f84e1a5893ad656
Urii Shalya <D.karaka@protonmail.com>
2019-12-08 20:03:57 +0200

    Add shooting trap to the game

---
+++ Assets/Scripts/ShootingTrap.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootingTrap : MonoBehaviour
+{
+    [SerializeField] private GameObject projectilePrefab;
+    private GameObject _projectile;
+    public float projectileSpeed = 3.0f;
+    public float shootFrequency = 2.0f;
+    public float destroyDelay = 3.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine("Shoot");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+        IEnumerator Shoot (){
+            yield return new WaitForSeconds(shootFrequency);
+            _projectile = Instantiate(projectilePrefab) as GameObject;
+            _projectile.transform.position = this.transform.position;
+            _projectile.transform.rotation = this.transform.rotation;
+            _projectile.GetComponent<Rigidbody2D>().AddForce(Vector2.left * projectileSpeed);
+            Destroy(_projectile, destroyDelay);
+            StartCoroutine("Shoot");
+        }
+    }
