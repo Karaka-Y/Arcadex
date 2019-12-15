3e9243bbd3f414aa377472d1677779c532b80fc0
Urii Shalya <D.karaka@protonmail.com>
2019-12-15 19:35:34 +0200

    Improving trap behavior

---
+++ Assets/Scripts/Hurt.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hurt : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnTriggerEnter2D(Collider2D other){
+            isDestroyable target = other.GetComponent<isDestroyable>();
+            if (target != null){
+                other.transform.SetPositionAndRotation( new Vector3(-3.07f, -0.994f, 0), other.transform.rotation);
+            }
+        }
+}
--- Assets/Scripts/ShootingTrap.cs
+++ Assets/Scripts/ShootingTrap.cs
@@ -9,9 +9,11 @@ public class ShootingTrap : MonoBehaviour
     public float projectileSpeed = 3.0f;
     public float shootFrequency = 2.0f;
     public float destroyDelay = 3.0f;
+    private Vector2 shootDirection;
     // Start is called before the first frame update
     void Start()
     {
+        ShootDirection();
         StartCoroutine("Shoot");
     }
 
@@ -20,12 +22,26 @@ public class ShootingTrap : MonoBehaviour
     {
        
     }
+    private void ShootDirection(){
+        if (this.transform.rotation.eulerAngles.y == 0 && this.transform.rotation.eulerAngles.z == 0){
+            shootDirection = Vector2.left;
+        }
+        else if (this.transform.rotation.eulerAngles.y == 180.0f && this.transform.rotation.eulerAngles.z == 0){
+            shootDirection = Vector2.right;
+        }
+        else if(this.transform.rotation.eulerAngles.y == 0 && this.transform.rotation.eulerAngles.z == 90){
+            shootDirection = Vector2.down;
+        }
+        else if (this.transform.rotation.eulerAngles.y == 0 && this.transform.rotation.eulerAngles.z == 270){
+            shootDirection = Vector2.up;
+        }
+    }
         IEnumerator Shoot (){
             yield return new WaitForSeconds(shootFrequency);
             _projectile = Instantiate(projectilePrefab) as GameObject;
             _projectile.transform.position = this.transform.position;
             _projectile.transform.rotation = this.transform.rotation;
-            _projectile.GetComponent<Rigidbody2D>().AddForce(Vector2.left * projectileSpeed);
+            _projectile.GetComponent<Rigidbody2D>().AddForce(shootDirection * projectileSpeed);
             Destroy(_projectile, destroyDelay);
             StartCoroutine("Shoot");
         }
