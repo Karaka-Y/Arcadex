5394e0a7474af313a2abd545d14821065f0842e4
Urii Shalya <D.karaka@protonmail.com>
2020-01-19 21:48:24 +0200

    Added some animations to main menu

--- Assets/Scripts/UI_Manager.cs
+++ Assets/Scripts/UI_Manager.cs
@@ -5,8 +5,18 @@ using UnityEngine.SceneManagement;
 
 public class UI_Manager : MonoBehaviour
 {
+    public Animator startButton;
+    public Animator settingsButton;
+    public Animator exitButton;
     public void StartGame()
     {
         SceneManager.LoadScene("SampleScene");
+    }
+
+    public void SettingsOpen()
+    {
+        startButton.SetBool("isHidden", true);
+        settingsButton.SetBool("isHidden", true);
+        exitButton.SetBool("isHidden", true);
     }
 }
