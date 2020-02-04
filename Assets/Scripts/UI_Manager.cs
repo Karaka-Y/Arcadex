a116b157499b90bfc2043cb1acb2814c887af9bb
Urii Shalya <D.karaka@protonmail.com>
2020-02-04 22:52:11 +0200

    Added sound and control

--- Assets/Scripts/UI_Manager.cs
+++ Assets/Scripts/UI_Manager.cs
@@ -8,6 +8,7 @@ public class UI_Manager : MonoBehaviour
     public Animator startButton;
     public Animator settingsButton;
     public Animator exitButton;
+    public Animator dialog;
     public void StartGame()
     {
         SceneManager.LoadScene("SampleScene");
@@ -18,5 +19,13 @@ public class UI_Manager : MonoBehaviour
         startButton.SetBool("isHidden", true);
         settingsButton.SetBool("isHidden", true);
         exitButton.SetBool("isHidden", true);
+        dialog.SetBool("isHidden", false);
+    }
+    public void CloseSettings(){
+        dialog.SetBool("isHidden", true);
+        startButton.SetBool("isHidden", false);
+        settingsButton.SetBool("isHidden", false);
+        exitButton.SetBool("isHidden", false);
+        
     }
 }
