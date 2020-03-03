4363a02f8d6b4b354955783c95b2a76c75a624fb
Urii Shalya <D.karaka@protonmail.com>
2020-03-03 23:48:48 +0200

    Little plugins added

---
+++ Assets/Scripts/Item.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Item", menuName = "Item")]
+public class Item : ScriptableObject
+{
+    public int Id;
+    public new string name;
+    public string description;
+    public Sprite ItemIcon;
+
+}
---
+++ Assets/Scripts/ItemCollect.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemCollect : MonoBehaviour
+{
+    private GameObject _item;
+
+    void OnTriggerEnter2D(Collider2D other){
+        if (other.tag == "Item"){
+            _item = other.gameObject;
+            Collect();
+            _item.SetActive(false);
+        }
+    }
+
+    private void Collect (){
+        Debug.Log("Item picked up");
+    }
+}
